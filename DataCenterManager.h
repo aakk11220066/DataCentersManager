a1799c9956ad63aca0762485e58d759e04dfdad3
klorin <klorin.roi@cqmpus.technion.ac.il>
2019-12-09 01:39:36 +0200

    fixed RequestServer FreeServer bug

--- DataCenterManager.h
+++ DataCenterManager.h
@@ -9,17 +9,22 @@
 #include "DataCenter.h"
 #include "AuxDataCenter.h"
 
-class DataCenterManager{
+class DataCenterManager {
 private:
     AVLTree<DataCenter> data_centers_tree;
     AVLTree<AuxDataCenter> linux_tree;
     AVLTree<AuxDataCenter> windows_tree;
 public:
-    enum DataCenterManagerError{SUCCESS = 0, ERROR = -1};
+    enum DataCenterManagerError {
+        SUCCESS = 0, ERROR = -1
+    };
+
     DataCenterManager() = default;
+
     ~DataCenterManager() = default;
-    DataCenterManagerError AddDataCenter(int DataCenterId, int numOfServers){
-        if ((DataCenterId <= 0) || (numOfServers <=0)) return ERROR;
+
+    DataCenterManagerError AddDataCenter(int DataCenterId, int numOfServers) {
+        if ((DataCenterId <= 0) || (numOfServers <= 0)) return ERROR;
         try {
             DataCenter dc_temp(DataCenterId, numOfServers);
             data_centers_tree.insert(dc_temp);
@@ -27,14 +32,14 @@ public:
             linux_tree.insert(aux_dc_temp);
             return SUCCESS;
         }
-        catch (DataManagerExceptions::Exceptions& e)
-        {
+        catch (DataManagerExceptions::Exceptions &e) {
             return ERROR;
         }
     }
-    DataCenterManagerError RemoveDataCenter(int DataCenterId){
-        if (DataCenterId <=0) return ERROR;
-        try{
+
+    DataCenterManagerError RemoveDataCenter(int DataCenterId) {
+        if (DataCenterId <= 0) return ERROR;
+        try {
             DataCenter dc_temp(DataCenterId);
             DataCenter dc_to_delete = data_centers_tree.find(dc_temp);
             if (!dc_to_delete.linux_size) {
@@ -48,52 +53,57 @@ public:
             data_centers_tree.remove(dc_temp);
             return SUCCESS;
         }
-        catch(DataManagerExceptions::Exceptions& e){
+        catch (DataManagerExceptions::Exceptions &e) {
             return ERROR;
         }
     }
 
-    DataCenterManagerError RequestServer(int DataCenterID, int ServerID, int os, int *assignedID){
+    DataCenterManagerError RequestServer(int DataCenterID, int ServerID, int os, int *assignedID) {
         try {
             DataCenter dc_temp(DataCenterID);
-            DataCenter dc_to_alter = data_centers_tree.find(dc_temp);
-            DataCenter dc_before_change = dc_to_alter;
+            DataCenter &dc_to_alter = data_centers_tree.find(dc_temp);
+            AuxDataCenter linux_dc_before_change(dc_to_alter, 0);
+            AuxDataCenter windows_dc_before_change(dc_to_alter, 1);
             if (dc_to_alter.requestServer(os, ServerID, assignedID) != 0) return ERROR;
-            printf("hjghjgh\n");
-            UpdateTrees(dc_before_change, dc_to_alter);
+            AuxDataCenter linux_dc_after_change(dc_to_alter, 0);
+            AuxDataCenter windows_dc_after_change(dc_to_alter, 1);
+            UpdateTrees(linux_dc_before_change, windows_dc_before_change, linux_dc_after_change,
+                        windows_dc_after_change, dc_to_alter.linux_size, dc_to_alter.windows_size);
             return SUCCESS;
         }
-        catch(DataManagerExceptions::Exceptions& e){
+        catch (DataManagerExceptions::Exceptions &e) {
             return ERROR;
         }
     }
 
-    DataCenterManagerError FreeServer(int DataCenterID, int ServerID){
-        try{
+    DataCenterManagerError FreeServer(int DataCenterID, int ServerID) {
+        try {
             DataCenter dc_temp(DataCenterID);
-            DataCenter dc_to_alter = data_centers_tree.find(dc_temp);
+            DataCenter &dc_to_alter = data_centers_tree.find(dc_temp);
+            AuxDataCenter linux_dc_before_change(dc_to_alter, 0);
+            AuxDataCenter windows_dc_before_change(dc_to_alter, 1);
             if (!dc_to_alter.freeServer(ServerID)) return ERROR;
-            DataCenter dc_before_change = dc_to_alter;
-            UpdateTrees(dc_before_change, dc_to_alter);
+            AuxDataCenter linux_dc_after_change(dc_to_alter, 0);
+            AuxDataCenter windows_dc_after_change(dc_to_alter, 1);
+            UpdateTrees(linux_dc_before_change, windows_dc_before_change, linux_dc_after_change,
+                        windows_dc_after_change, dc_to_alter.linux_size, dc_to_alter.windows_size);
             return SUCCESS;
 
         }
-        catch(DataManagerExceptions::Exceptions& e){
+        catch (DataManagerExceptions::Exceptions &e) {
             return ERROR;
         }
     }
 
-    void UpdateTrees(const DataCenter& dc_to_delete, const DataCenter& dc_to_add){
-        AuxDataCenter linux_dc_to_delete(dc_to_delete, 0);
-        if (dc_to_delete.linux_size) linux_tree.remove(linux_dc_to_delete);
-        if (dc_to_add.linux_size){
-            AuxDataCenter linux_dc_to_add(dc_to_add, 0);
+    void UpdateTrees(const AuxDataCenter &linux_dc_to_delete, const AuxDataCenter &windows_dc_to_delete,
+                     const AuxDataCenter &linux_dc_to_add, const AuxDataCenter &windows_dc_to_add, int linux_size, int windows_size) {
+        if (linux_dc_to_add.getServersNum()) linux_tree.remove(linux_dc_to_delete);
+        if (linux_size) {
             linux_tree.insert(linux_dc_to_add);
         }
-        AuxDataCenter windows_dc_to_delete(dc_to_delete, 1);
-        if (dc_to_delete.windows_size) windows_tree.remove(windows_dc_to_delete);
-        if (dc_to_add.windows_size){
-            AuxDataCenter windows_dc_to_add(dc_to_add, 0);
+
+        if (linux_dc_to_add.getServersNum()) windows_tree.remove(windows_dc_to_delete);
+        if (windows_size) {
             windows_tree.insert(windows_dc_to_add);
         }
     }
@@ -106,7 +116,7 @@ public:
                 Array<AuxDataCenter> list0 = linux_tree.getInOrder();
                 if (!list0.getSize()) return ERROR;
                 int size0 = (int) list0.getSize();
-                *dataCenters = (int*)malloc(sizeof(int)*size0);
+                *dataCenters = (int *) malloc(sizeof(int) * size0);
                 //printf("size is %d\n", size0* sizeof(int));
                 //printf("size0 is %d\n", size0);
                 for (int i = 0; i < size0; i++) {
@@ -126,7 +136,7 @@ public:
             }
             return SUCCESS;
         }
-        catch(DataManagerExceptions::Exceptions& e){
+        catch (DataManagerExceptions::Exceptions &e) {
             return ERROR;
         }
     }
